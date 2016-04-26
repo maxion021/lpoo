7c1e903dd11d2690315fd63267a0c5177a4fdbb4
Douglas Carlos <douglascsoliveira@hotmail.com>
2016-04-25 23:58:11 -0300

    Arquivos iniciais

---
+++ Dado.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jogodedados;
+
+/**
+ *
+ * @author Douglas Carlos da  Silva Oliveira
+ */
+public class Dado {
+    int valor_dado = 0;
+    
+
+}
---
+++ Jogo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jogodedados;
+
+/**
+ *
+ * @author Douglas Carlos da  Silva Oliveira
+ */
+public class Jogo {
+
+}
