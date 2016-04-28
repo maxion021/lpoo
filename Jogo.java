57eb5d2236eb37a822498e3de4cdd86c9f85ccc4
Douglas Carlos <douglascsoliveira@hotmail.com>
2016-04-28 00:53:15 -0300

    Criação da função primeiraJogada

--- Jogo.java
+++ Jogo.java
@@ -36,6 +36,24 @@ public class Jogo {
         System.out.println("Vitória");
     }
     
-    
+    void primeiraJogada(){
+        dado1.gerar();
+        dado2.gerar();
+        soma = (dado1.valor_dado+dado2.valor_dado);
+        if(soma == 7 || soma == 11){
+            System.out.println("Dados: "+dado1.valor_dado+" + "+dado2.valor_dado+" = " + soma);
+            System.out.println("Vitoria");
+        }
+
+        if(soma == 2 || soma == 3 || soma == 12){
+            System.out.println("Dados: "+dado1.valor_dado+" + "+dado2.valor_dado+" = " + soma);
+            System.out.println("Derrota");
+        }
+        
+        if(soma == 4 || soma == 5 || soma == 6 || soma == 8 || soma == 9 || soma == 10){
+            System.out.println("Dados: "+dado1.valor_dado+" + "+dado2.valor_dado+" = " + soma);
+            System.out.println("Pontuação: " + soma);
+        }
+    }
 
 }
