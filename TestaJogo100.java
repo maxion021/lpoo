f6438b764625ee75a2b8fbbc41ac635f1779f961
douglascsooliveira <douglascsoliveira@hotmail.com>
2016-05-01 23:54:16 -0300

    Update TestaJogo100

--- TestaJogo100.java
+++ TestaJogo100.java
@@ -6,19 +6,27 @@
 
 package jogodedados;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Douglas Carlos da  Silva Oliveira
  */
 public class TestaJogo100 {
+
+    int[][] c = new int[2][100];
+    int[] d = new int[2];
     
     int variosJogos(){
         Jogo partida = new Jogo();
-
-        for(int cont=1 ; cont <= 1000 ; cont++){
+        for(int cont=0 ; cont < 100 ; cont++){
     System.out.println(cont);
-            partida.jogada();
+            d = partida.jogada();
+            c[0][cont] = d[0];
+            c[1][cont] = d[1];
        }
+        System.out.println(Arrays.toString(c[0]));
+        System.out.println(Arrays.toString(c[1]));
         return 0;
     }
 }
